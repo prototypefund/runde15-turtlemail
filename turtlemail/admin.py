1f3ebe91f797a8506da5c71feefe3940742b74d1
Veronika Plass <veronika.plass@unicode-it.de>
2024-04-08 09:09:58 +0200

    chore: add django user admin

---
+++ turtlemail/admin.py
@@ -0,0 +1,54 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as _UserAdmin
+
+from .forms import UserCreationForm
+from .models import User
+
+
+class UserAdmin(_UserAdmin):
+    add_form = UserCreationForm
+    model = User
+    list_display = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("email", "password", "username"),
+            },
+        ),
+        (
+            "Permissions",
+            {"fields": ("is_staff", "is_active", "groups", "user_permissions")},
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "username",
+                    "is_staff",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(User, UserAdmin)
