fb504aecbe4f820e59d1487a8fe4d30be09d9813
Rafael Epplée <hello@rafa.ee>
2024-04-09 13:19:15 +0200

    chore: Add packet and routing models

--- turtlemail/models.py
+++ turtlemail/models.py
@@ -138,3 +138,156 @@ class Stay(models.Model):
     class Meta:
         verbose_name = _("Stay")
         verbose_name_plural = _("Stays")
+
+
+class Packet(models.Model):
+    # Users with packets can't be deleted right now.
+    # What if the packet is still being delivered?
+    sender = models.ForeignKey(
+        User,
+        verbose_name=_("Sender"),
+        on_delete=models.RESTRICT,
+        related_name="sent_packets",
+    )
+    receiver = models.ForeignKey(
+        User,
+        verbose_name=_("Recipient"),
+        on_delete=models.RESTRICT,
+        related_name="received_packets",
+    )
+    created_at = models.DateTimeField(verbose_name=_("Created at"), auto_now_add=True)
+    human_id = models.TextField(verbose_name=_("Code"), unique=True)
+    current_route = models.OneToOneField(
+        "Route",
+        verbose_name=_("Current route"),
+        on_delete=models.RESTRICT,
+        # Don't create a field on the route pointing to this packet
+        related_name="+",
+    )
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["human_id"]),
+            models.Index(fields=["sender_id"]),
+            models.Index(fields=["receiver_id"]),
+        ]
+
+        verbose_name = _("Packet")
+        verbose_name_plural = _("Packets")
+
+    def __str__(self):
+        return f'Packet "{self.human_id}"'
+
+
+class Route(models.Model):
+    CURRENT = "CURRENT"
+    CANCELLED = "CANCELLED"
+
+    STATUS_CHOICES = [(CURRENT, "Current"), (CANCELLED, "Cancelled")]
+
+    status = models.TextField(verbose_name=_("Status"), choices=STATUS_CHOICES)
+    packet = models.ForeignKey(
+        Packet,
+        verbose_name=_("Packet"),
+        on_delete=models.CASCADE,
+        related_name="all_routes",
+    )
+
+    class Meta:
+        verbose_name = _("Route")
+        verbose_name_plural = _("Routes")
+
+    def __str__(self):
+        return f"{self.status} Route"
+
+
+class RouteStep(models.Model):
+    SUGGESTED = "SUGGESTED"
+    ACCEPTED = "ACCEPTED"
+    ONGOING = "ONGOING"
+    COMPLETED = "COMPLETED"
+    CANCELLED = "CANCELLED"
+
+    STATUS_CHOICES = [
+        (SUGGESTED, "Suggested"),
+        (ACCEPTED, "Accepted"),
+        (ONGOING, "Ongoing"),
+        (COMPLETED, "Completed"),
+        (CANCELLED, "Cancelled"),
+    ]
+
+    stay = models.ForeignKey(
+        Stay,
+        verbose_name=_("Stay"),
+        related_name="route_steps",
+        on_delete=models.RESTRICT,
+    )
+    start = models.DateField(
+        verbose_name=_("Start"),
+        validators=[MinValueValidator(limit_value=datetime.date.today)],
+        null=True,
+        blank=True,
+    )
+    end = models.DateField(
+        verbose_name=_("End"),
+        validators=[MinValueValidator(limit_value=datetime.date.today)],
+        null=True,
+        blank=True,
+    )
+    previous_step = models.OneToOneField(
+        "self",
+        verbose_name=_("Previous Step"),
+        on_delete=models.CASCADE,
+        related_name="+",
+        null=True,
+    )
+    next_step = models.OneToOneField(
+        "self",
+        verbose_name=_("Next Step"),
+        on_delete=models.CASCADE,
+        related_name="+",
+        null=True,
+    )
+    packet = models.ForeignKey(
+        Packet, verbose_name=_("Packet"), on_delete=models.CASCADE
+    )
+    status = models.TextField(verbose_name=_("Status"), choices=STATUS_CHOICES)
+    route = models.ForeignKey(
+        Route, verbose_name=_("Route"), on_delete=models.CASCADE, related_name="steps"
+    )
+
+    class Meta:
+        verbose_name = _("Route Step")
+        verbose_name_plural = _("Route Steps")
+
+
+class DeliveryLog(models.Model):
+    ROUTE_STEP_CHANGE = "ROUTE_STEP_CHANGE"
+    NEW_ROUTE = "NEW_ROUTE"
+
+    ACTION_CHOICES = (
+        (ROUTE_STEP_CHANGE, "Route Step Changed"),
+        (NEW_ROUTE, "New Route"),
+    )
+
+    created_at = models.DateTimeField(verbose_name=_("Datetime"))
+    route_step = models.ForeignKey(
+        RouteStep,
+        verbose_name=_("Route Step"),
+        on_delete=models.CASCADE,
+        related_name="delivery_logs",
+    )
+    packet = models.ForeignKey(
+        Packet, verbose_name=_("Packet"), on_delete=models.CASCADE, unique=False
+    )
+    route = models.ForeignKey(Route, verbose_name=_("Route"), on_delete=models.CASCADE)
+    action = models.TextField(choices=ACTION_CHOICES, verbose_name=_("Action Choices"))
+    new_step_status = models.TextField(
+        choices=RouteStep.STATUS_CHOICES,
+        verbose_name=_("New Route Step Status"),
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = _("Delivery Log Entry")
+        verbose_name_plural = _("Delivery Log Entries")
