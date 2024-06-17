3c38344fcb0ec10862dbfdf4d329788c76f88ead
Rafael Epplée <hello@rafa.ee>
2024-06-17 12:54:29 +0200

    feat: Add new logo and custom color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{js,ts,jsx,tsx,vue}',
-    './turtlemail/**/*.{html,jinja,js,py}',
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+    "./turtlemail/**/*.{html,jinja,js,py}",
   ],
   theme: {},
-  plugins: [require('daisyui')],
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    darkTheme: "turtlemail",
+    themes: [
+      {
+        turtlemail: {
+          ...require("daisyui/src/theming/themes")["cupcake"],
+          success: "rgb(59,103,59)",
+          primary: "rgb(59,103,59)",
+          secondary: "rgb(255,141,0)",
+          accent: "oklch(59.48% 0.2069 29.12)",
+        },
+      },
+    ],
+  },
+};
