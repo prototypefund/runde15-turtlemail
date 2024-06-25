5eb510dd5e7e975ce0fdd375fcaae2069ce2eb9a
Rafael Epplée <hello@rafa.ee>
2024-06-25 12:55:46 +0200

    chore: Configure prettier to match eslint formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,vue}",
-    "./turtlemail/**/*.{html,jinja,js,py}",
+    './src/**/*.{js,ts,jsx,tsx,vue}',
+    './turtlemail/**/*.{html,jinja,js,py}',
   ],
   theme: {},
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
-    darkTheme: "turtlemail",
+    darkTheme: 'turtlemail',
     themes: [
       {
         turtlemail: {
-          ...require("daisyui/src/theming/themes")["cupcake"],
-          success: "rgb(59,103,59)",
-          primary: "rgb(59,103,59)",
-          secondary: "rgb(255,141,0)",
-          accent: "oklch(59.48% 0.2069 29.12)",
+          ...require('daisyui/src/theming/themes').cupcake,
+          success: 'rgb(59,103,59)',
+          primary: 'rgb(59,103,59)',
+          secondary: 'rgb(255,141,0)',
+          accent: 'oklch(59.48% 0.2069 29.12)',
         },
       },
     ],
   },
-};
+}
